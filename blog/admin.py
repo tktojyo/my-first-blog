7d9aa79e0af7a9c02e9cd793ca68f0061c983495
tamaki tojo <tktojyo.bk@kaien-t.net>
2023-07-24 15:01:48 +0900

    コメント機能を追加

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
+from tokenize import Comment
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
